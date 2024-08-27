80c06e6a6944679a7582044462973ff7554adc03
hallanic-isep <herve.allanic@isep.fr>
2024-08-27 14:53:56 +0200

    petite modification...

--- juste_prix.py
+++ juste_prix.py
@@ -4,17 +4,21 @@ print("Trouver le prix de l'objet X..")
 cible = random.randint(1,10)
 #print(f"cible={cible}")
 
-# Récupération de la proposition du joueur
-essai = int(input("Votre proposition entre 1 et 10 : "))
+for i in range(5):
 
-if essai < 1 or essai > 10 :
-    print("Erreur, il faut saisir une valeur entre 1 et 10...")
-    exit()
+    # Récupération de la proposition du joueur
+    essai = int(input("Votre proposition entre 1 et 10 : "))
 
-if cible == essai:
-    print("Bravo !!!")
-elif cible < essai:
-    print("Trop élevé...")
-else:
-    print("Trop peu...")
+    if essai < 1 or essai > 10 :
+        print("Erreur, il faut saisir une valeur entre 1 et 10...")
+        exit()
 
+    if cible == essai:
+        print("Bravo !!!")
+        break
+    elif cible < essai:
+        print("Trop élevé...")
+    else:
+        print("Trop peu")
+
+print("Fin du jeu...")
